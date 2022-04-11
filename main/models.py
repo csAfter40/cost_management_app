f7aa62e055adbfbf31aec69d52ff5b18c509dbd4
csAfter40 <csafter40@gmail.com>
2022-04-11 08:35:55 +0300

    Created models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Currency(models.Model):
+    code = models.CharField(max_length=2)
+    name = models.CharField(max_length=32)
+    symbol = models.CharField(max_length=8, null=True, blank=True)
+
+    def __str__(self):
+        return self.code
+
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    balance = models.DecimalField(max_digits=14, decimal_places=2)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+
+
+class CostCategory(MPTTModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=64)
+    slug = models.SlugField()
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    class Meta:
+        unique_together = (('parent', 'slug'))
+        verbose_name_plural = 'CostCategories'
+
+
+class IncomeCategory(MPTTModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=64)
+    slug = models.SlugField()
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    class Meta:
+        unique_together = (('parent', 'slug'))
+        verbose_name_plural = 'IncomeCategories'
+
+
+class Cost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    amount = models.DecimalField(max_digits=14, decimal_places=2)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE, )
+
+
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    amount = models.DecimalField(max_digits=14, decimal_places=2)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE, )
