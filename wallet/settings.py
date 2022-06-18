363e8dd73f01b17598d0304094b3b3874ac946c5
csAfter40 <csafter40@gmail.com>
2022-06-18 22:58:47 +0300

    Added info for tables in case no data available

--- wallet/settings.py
+++ wallet/settings.py
@@ -29,7 +29,6 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-eu!c-g+di6twcl43e&!%81%kz2-rnvtgw9_0o5pf^8+j-bswf4'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
