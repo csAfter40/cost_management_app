d0ba542af156d335e2cffeb9972103d429cd4cca
csAfter40 <csafter40@gmail.com>
2022-07-14 23:09:29 +0300

    Edited admin panel options.

--- main/admin.py
+++ main/admin.py
@@ -10,12 +10,54 @@ from .models import (
     Loan,
 )
 
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Currency)
-admin.site.register(Account)
-admin.site.register(Category)
-admin.site.register(Transaction)
-admin.site.register(Transfer)
-admin.site.register(UserPreferences)
-admin.site.register(Loan)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    ordering = ('username',)
+
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'symbol')
+    ordering = ('code',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'type')
+    ordering = ('user__username', 'type', 'name')
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'is_active')
+    ordering = ('user__username', '-is_active', 'name')
+
+
+@admin.register(Loan)
+class LoanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'is_active')
+    ordering = ('user__username', '-is_active', 'name')
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display= ('name', 'amount', 'account', 'date', 'get_user')
+    ordering = ('-date',)
+
+    def get_user(self, obj):
+        return obj.account.user
+
+    # sets column name as 'user' in admin panel. otherwise it was 'get_user'
+    get_user.short_description = 'user'
+
+@admin.register(Transfer)
+class TransferAdmin(admin.ModelAdmin):
+    list_display = ('user', 'from_transaction', 'to_transaction', 'date')
+    ordering = ('-date',)
+
+
+@admin.register(UserPreferences)
+class UserPreferencesAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    ordering = ('user__username',)
