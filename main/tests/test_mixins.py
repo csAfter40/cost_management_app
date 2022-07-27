aa8fa055bbe0e0f216655c8f9653c640af2413a5
csAfter40 <csafter40@gmail.com>
2022-07-27 10:15:48 +0300

    Added BaseViewTestMixin.

---
+++ main/tests/test_mixins.py
@@ -0,0 +1,82 @@
+import logging
+from django.core.exceptions import ImproperlyConfigured
+from django.contrib.auth.models import AnonymousUser
+from django.urls import resolve
+
+
+class BaseViewTestMixin(object):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_url = None # str
+        cls.redirect_url = None # str 
+        cls.model = None # model.Model
+        cls.context_list = None # List of strings
+        cls.template = None # str 'app_name/template_name.html'
+        cls.post_method = False
+        cls.get_method = True
+        cls.post_data = None # List of dictionaries
+        cls.view_function = None # Add .as_view()
+        cls.login_required = False # bool
+        cls.user_factory = None
+
+    def setUp(self) -> None:
+        self.user = self.get_user()
+        if not self.test_url:
+            raise ImproperlyConfigured('No test url available. Please provide a test_url')
+        if self.login_required:
+            self.client.force_login(self.user)
+
+    def get_user(self):
+        user = self.user_factory() if self.user_factory else AnonymousUser()
+        return user
+
+    def test_unauthenticated_access(self):
+        '''
+            Tests unauthenticated access in case view has LoginRequired mixin.
+        '''
+        if not self.login_required:
+            return
+        self.client.logout()
+        response = self.client.get(self.test_url)
+        self.assertEquals(response.status_code, 302)
+
+    def test_get(self):    
+        '''
+            Tests get request response has status 200 and 
+            response context has expected keys.
+        ''' 
+        if not self.get_method:
+            return
+        response = self.client.get(self.test_url)
+        self.assertEquals(response.status_code, 200)
+        # test template
+        if self.template:
+            self.assertTemplateUsed(response, self.template)
+        else:
+            logging.warning('\nWarning: No template available. Template test not implemented.')
+        # test context
+        if self.context_list:
+            for item in self.context_list:
+                self.assertIn(item, response.context.keys())
+        else:
+            logging.warning('\nWarning: No context_list available. Context test not implemented.')
+
+    def test_post(self):
+        if not self.post_method:
+            return
+        response = self.client.post(self.test_url, self.post_data)
+        if self.redirect_url:
+            self.assertRedirects(response, self.redirect_url, 302, fetch_redirect_response=False)
+        else:
+            self.assertEquals(response.status_code, 200)
+        return response
+
+    def test_view_function(self):
+        '''
+            Tests url resolves to view function.
+        '''
+        if not self.view_function:
+            raise ImproperlyConfigured('No view function available. Please provide a view_function.')
+        match = resolve(self.test_url)
+        self.assertEquals(self.view_function.__name__, match.func.__name__)
