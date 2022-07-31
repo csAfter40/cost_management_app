d497b3c4bd527864b42276259e8151a4e38605af
csAfter40 <csafter40@gmail.com>
2022-07-31 10:36:46 +0300

    Added factory tests.

---
+++ main/tests/tests_factories.py
@@ -0,0 +1,46 @@
+from main.models import (
+    User, Currency, Account, Loan, Category, Transaction,
+    Transfer,
+)
+from main.tests.factories import (
+    UserFactoryNoSignal, CurrencyFactory, AccountFactory, 
+    LoanFactory, CategoryFactory, TransactionFactory, TransferFactory
+)
+from main.tests.test_mixins import BaseFactoryTestMixin
+from django.test import TestCase
+
+class TestUserFactoryNoSignal(BaseFactoryTestMixin, TestCase):
+    model = User
+    factory_class = UserFactoryNoSignal
+
+
+class TestCurrencyFactoryNoSignal(BaseFactoryTestMixin, TestCase):
+    model = Currency
+    factory_class = CurrencyFactory
+
+
+class TestAccountFactoryNoSignal(BaseFactoryTestMixin, TestCase):
+    model = Account
+    factory_class = AccountFactory
+
+
+class TestLoanFactoryNoSignal(BaseFactoryTestMixin, TestCase):
+    model = Loan
+    factory_class = LoanFactory
+
+
+class TestCategoryFactoryNoSignal(BaseFactoryTestMixin, TestCase):
+    model = Category
+    factory_class = CategoryFactory
+
+    def setUp(self):
+        self.object = CategoryFactory(parent=None)
+
+class TestTransactionFactoryNoSignal(BaseFactoryTestMixin, TestCase):
+    model = Transaction
+    factory_class = TransactionFactory
+
+
+class TestTransferFactoryNoSignal(BaseFactoryTestMixin, TestCase):
+    model = Transfer
+    factory_class = TransferFactory
