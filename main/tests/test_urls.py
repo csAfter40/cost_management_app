06bebb9daed06e20c462ac50e6872940f7ac88be
csAfter40 <csafter40@gmail.com>
2022-08-19 14:15:53 +0300

    Added url tests.

--- main/tests/test_urls.py
+++ main/tests/test_urls.py
@@ -12,23 +12,48 @@ class TestUrls(SimpleTestCase):
         found = resolve(path)
         self.assertEquals(found.func, view_func)
 
-    def test_index(self):
+    def test_index_url(self):
         self.assert_path_resolves_to_FBV("/", views.index)
 
-    def test_login(self):
+    def test_login_url(self):
         self.assert_path_resolves_to_CBV("/login", views.LoginView)
 
-    def test_logout(self):
+    def test_logout_url(self):
         self.assert_path_resolves_to_FBV("/logout", views.logout_view)
 
-    def test_register(self):
+    def test_register_url(self):
         self.assert_path_resolves_to_CBV("/register", views.RegisterView)
 
-    def test_accounts(self):
+    def test_accounts_url(self):
         self.assert_path_resolves_to_CBV("/accounts", views.AccountsView)
 
-    def test_accounts_create(self):
+    def test_account_create_url(self):
         self.assert_path_resolves_to_CBV("/accounts/create", views.CreateAccountView)
 
-    def test_accounts_delete(self):
+    def test_account_delete_url(self):
         self.assert_path_resolves_to_CBV("/accounts/del/1", views.DeleteAccountView)
+
+    def test_account_detail_ajax_url(self):
+        self.assert_path_resolves_to_CBV(
+            "/accounts/1/ajax/1", views.AccountDetailAjaxView
+        )
+
+    def test_account_detail_subcategory_ajax_url(self):
+        self.assert_path_resolves_to_CBV(
+            "/accounts/1/ajax/1", views.AccountDetailSubcategoryAjaxView
+        )
+
+    def test_account_detail_url(self):
+        self.assert_path_resolves_to_CBV("/accounts/1", views.AccountDetailView)
+
+    def test_account_edit_url(self):
+        self.assert_path_resolves_to_CBV("/accounts/1/edit", views.EditAccountView)
+
+    def test_loans_url(self):
+        self.assert_path_resolves_to_CBV("/loans", views.LoansView)
+
+    def test_loan_create_url(self):
+        self.assert_path_resolves_to_CBV("/loans/create", views.CreateLoanView)
+
+    def test_loan_delete_url(self):
+        self.assert_path_resolves_to_CBV("/loans/del/1", views.DeleteLoanView)
