3cfe5d2cd7bf068920852243e41d59595789700e
csAfter40 <csafter40@gmail.com>
2022-08-19 07:47:45 +0300

    Added helper functions to TestUrls class.

--- main/tests/test_urls.py
+++ main/tests/test_urls.py
@@ -1,36 +1,34 @@
-from django.test.testcases import TestCase, SimpleTestCase
+from django.test.testcases import SimpleTestCase
 from django.urls import resolve
 from main import views
 
+
 class TestUrls(SimpleTestCase):
+    def assert_path_resolves_to_CBV(self, path, view_class):
+        found = resolve(path)
+        self.assertEquals(found.func.__name__, view_class.as_view().__name__)
+
+    def assert_path_resolves_to_FBV(self, path, view_func):
+        found = resolve(path)
+        self.assertEquals(found.func, view_func)
 
     def test_index(self):
-        found = resolve('/')
-        self.assertEquals(found.func, views.index)
+        self.assert_path_resolves_to_FBV("/", views.index)
 
     def test_login(self):
-        found = resolve('/login')
-        self.assertEquals(
-            found.func.__name__, 
-            views.LoginView.as_view().__name__
-        )
+        self.assert_path_resolves_to_CBV("/login", views.LoginView)
 
     def test_logout(self):
-        found = resolve('/logout')
-        self.assertEquals(found.func, views.logout_view)
+        self.assert_path_resolves_to_FBV("/logout", views.logout_view)
 
     def test_register(self):
-        found = resolve('/register')
-        self.assertEquals(
-            found.func.__name__, 
-            views.RegisterView.as_view().__name__
-        )
-    
-    def test_accounts(self):
-        found = resolve('/accounts')
-        self.assertEquals(
-            found.func.__name__, 
-            views.AccountsView.as_view().__name__
-        )
+        self.assert_path_resolves_to_CBV("/register", views.RegisterView)
 
-    
+    def test_accounts(self):
+        self.assert_path_resolves_to_CBV("/accounts", views.AccountsView)
+
+    def test_accounts_create(self):
+        self.assert_path_resolves_to_CBV("/accounts/create", views.CreateAccountView)
+
+    def test_accounts_delete(self):
+        self.assert_path_resolves_to_CBV("/accounts/del/1", views.DeleteAccountView)
