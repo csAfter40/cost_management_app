13ae81410298e4193a51b88ea58ebdf925701e34
csAfter40 <csafter40@gmail.com>
2022-10-18 13:05:36 +0300

    chore: add path for transactions month archive

--- main/urls.py
+++ main/urls.py
@@ -90,6 +90,11 @@ urlpatterns = [
         name='transactions_year_archive'
     ),
     path(
+        'transactions/date/<int:year>/<int:month>',
+        views.TransactionsMonthArchiveView.as_view(),
+        name='transactions_month_archive'
+    ),
+    path(
         "transactions/<int:pk>/edit",
         views.EditTransactionView.as_view(),
         name="edit_transaction",
