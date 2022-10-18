ba55328592ca84e9d2f21fd74a1fcc483ef0e87e
csAfter40 <csafter40@gmail.com>
2022-10-18 13:06:14 +0300

    feat: add transactions month archive view

--- main/views.py
+++ main/views.py
@@ -805,6 +805,21 @@ class TransactionsYearArchiveView(LoginRequiredMixin, YearArchiveView):
         return super().get_queryset().filter(content_type__model='account', object_id__in=user_accounts_list)
 
 
+class TransactionsMonthArchiveView(LoginRequiredMixin, MonthArchiveView):
+    model = Transaction
+    date_field = 'date'
+    paginate_by = settings.DEFAULT_PAGINATION_QTY
+    allow_future = True
+    allow_empty = True
+    extra_context = {'table_template': 'main/table_transactions.html'}
+    template_name = 'main/group_table_paginator.html'
+    month_format='%m'
+
+    def get_queryset(self):
+        user_accounts_list = Account.objects.filter(user=self.request.user).values_list('pk', flat=True)
+        return super().get_queryset().filter(content_type__model='account', object_id__in=user_accounts_list)    
+
+
 class EditTransactionView(LoginRequiredMixin, UpdateView):
     model = Transaction
     form_class = EditTransactionForm
