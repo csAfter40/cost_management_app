5771762cf39b34e6f686bafdd0fbf47288dcfbfb
csAfter40 <csafter40@gmail.com>
2022-10-18 13:16:03 +0300

    test: add test for transactions day archive url

--- main/tests/test_urls.py
+++ main/tests/test_urls.py
@@ -182,6 +182,9 @@ class TestUrls(SimpleTestCase):
     def test_transactions_week_archive_url(self):
         self.assert_path_resolves_to_CBV('/transactions/date/2001/week/1', views.TransactionsWeekArchiveView, 'transactions_week_archive', year=2001, week=1)
     
+    def test_transactions_day_archive_url(self):
+        self.assert_path_resolves_to_CBV('/transactions/date/2001/1/1', views.TransactionsDayArchiveView, 'transactions_day_archive', year=2001, month=1, day=1)
+
     def test_edit_transaction_url(self):
         self.assert_path_resolves_to_CBV("/transactions/1/edit", views.EditTransactionView, "edit_transaction", pk=1)
 
