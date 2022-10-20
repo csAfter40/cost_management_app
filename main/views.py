db5ab77c759258fa7155c72bbb97daf679680104
csAfter40 <csafter40@gmail.com>
2022-10-20 14:58:40 +0300

    feat: add ins outs all archive view

--- main/views.py
+++ main/views.py
@@ -1072,3 +1072,42 @@ class InsOutsView(LoginRequiredMixin, TemplateView):
         }
         context.update(extra_context)
         return context
+
+
+class InsOutsAllArchiveView(LoginRequiredMixin, ArchiveIndexView):
+    model = Transaction
+    date_field = 'date'
+    paginate_by = settings.DEFAULT_PAGINATION_QTY
+    allow_future = True
+    allow_empty = True
+    context_object_name = 'transactions'
+    template_name = 'main/group_report_chart.html'
+
+    def get_queryset(self):
+        user_accounts_list = Account.objects.filter(user=self.request.user).values_list('pk', flat=True)
+        return super().get_queryset().filter(content_type__model='account', object_id__in=user_accounts_list)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        transactions = self.get_dated_items()[1]
+        expense_category_stats = get_category_stats(
+            transactions, "E", None, self.request.user
+        )
+        income_category_stats = get_category_stats(
+            transactions, "I", None, self.request.user
+        )
+        comparison_stats = get_comparison_stats(
+            expense_category_stats, income_category_stats
+        )
+        report = get_ins_outs_report(self.request.user, transactions)
+        total = get_report_total(report, self.request.user.primary_currency)
+
+        extra_context = {
+            "expense_stats": expense_category_stats,
+            "income_stats": income_category_stats,
+            "comparison_stats": comparison_stats,
+            "report": report,
+            "total": total
+        }
+        context.update(extra_context)
+        return context
