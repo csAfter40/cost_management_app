ca780def4a134d3adbe322f02323de80f8d03da5
csAfter40 <csafter40@gmail.com>
2022-10-23 07:21:50 +0300

    chore: add mixin for category date archive view test

--- main/view_mixins.py
+++ main/view_mixins.py
@@ -1,5 +1,14 @@
 from .models import Account, Category
-from .utils import get_category_stats, get_comparison_stats, get_ins_outs_report, get_report_total
+from .utils import (
+    get_category_stats, 
+    get_comparison_stats, 
+    get_ins_outs_report, 
+    get_report_total,
+    get_subcategory_stats
+)
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+
 
 class InsOutsDateArchiveMixin():
     def get_queryset(self):
@@ -34,3 +43,25 @@ class InsOutsDateArchiveMixin():
         }
         context.update(extra_context)
         return context
+
+
+class CategoryDateArchiveMixin():
+    def get_queryset(self):
+        user_accounts_list = Account.objects.filter(user=self.request.user).values_list('pk', flat=True)
+        user_transfer_categories = Category.objects.filter(user=self.request.user, is_transfer=True)
+        return super().get_queryset().filter(
+            content_type__model='account', 
+            object_id__in=user_accounts_list
+            ).exclude(category__in=user_transfer_categories)
+
+    def get_category(self,category_id):
+        return get_object_or_404(Category, pk=category_id)
+
+    def get(self, request, *args, **kwargs):
+        if request.GET.get('ajax', None):
+            qs = self.get_dated_items()[1]
+            category_id = kwargs.get("pk")
+            category = self.get_category(category_id)
+            data = get_subcategory_stats(qs, category)
+            return JsonResponse(data)
+        return super().get(request, *args, **kwargs)
