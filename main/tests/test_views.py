38ae33206f3e0f5a4ee78c5129ad7a09c2d77b2f
csAfter40 <csafter40@gmail.com>
2022-10-23 07:46:02 +0300

    test: add test for category month archive view

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -2368,3 +2368,30 @@ class TestCategoryYearArchiveView(BaseViewTestMixin, TestCase):
         response = self.client.get(test_url)      
         self.assertEquals(response.status_code, 200)
         self.assertEqual(response['content-type'], 'application/json')
+
+
+class TestCategoryMonthArchiveView(BaseViewTestMixin, TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.context_list = ['transactions']
+        cls.template = 'main/category_detail.html'
+        cls.view_function = views.CategoryMonthArchiveView.as_view()
+        cls.login_required = True
+        cls.user_factory = UserFactoryNoSignal
+
+    def setUp(self) -> None:
+        self.user = self.get_user()
+        self.object = CategoryFactory(user=self.user, parent=None)
+        self.test_url = reverse(
+            'main:category_month_archive', 
+            kwargs={'pk': self.object.id, 'year': 2001, 'month': 1}
+        )
+        if self.login_required:
+            self.client.force_login(self.user)
+
+    def test_get_ajax(self):
+        test_url = self.test_url + '?ajax=1'
+        response = self.client.get(test_url)      
+        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response['content-type'], 'application/json')
