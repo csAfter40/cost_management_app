1d0722e16acfe7f0f6330337b5727b0742f751e4
csAfter40 <csafter40@gmail.com>
2022-10-23 07:55:56 +0300

    feat: add category day archive view

--- main/views.py
+++ main/views.py
@@ -1173,3 +1173,14 @@ class CategoryWeekArchiveView(CategoryDateArchiveMixin, LoginRequiredMixin, Arch
     allow_empty = True
     context_object_name = 'transactions'
     template_name = 'main/category_detail.html'
+
+
+class CategoryDayArchiveView(CategoryDateArchiveMixin, LoginRequiredMixin, ArchiveIndexView):
+    model = Transaction
+    date_field = 'date'
+    paginate_by = settings.DEFAULT_PAGINATION_QTY
+    allow_future = True
+    allow_empty = True
+    context_object_name = 'transactions'
+    template_name = 'main/category_detail.html'
+    month_format='%m'
