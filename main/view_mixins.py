e3e64f67713ae847c46cd7b2d30e67e164c815bb
csAfter40 <csafter40@gmail.com>
2022-10-25 11:25:21 +0300

    chore: edit category date archive mixin

--- main/view_mixins.py
+++ main/view_mixins.py
@@ -49,19 +49,28 @@ class CategoryDateArchiveMixin():
     def get_queryset(self):
         user_accounts_list = Account.objects.filter(user=self.request.user).values_list('pk', flat=True)
         user_transfer_categories = Category.objects.filter(user=self.request.user, is_transfer=True)
+        descendant_categories = self.get_category_descendants()
         return super().get_queryset().filter(
             content_type__model='account', 
-            object_id__in=user_accounts_list
+            object_id__in=user_accounts_list,
+            category__in=descendant_categories
             ).exclude(category__in=user_transfer_categories)
 
-    def get_category(self,category_id):
+    def get_category(self):
+        category_id = self.kwargs.get("pk")
         return get_object_or_404(Category, pk=category_id)
+
+    def get_category_descendants(self):
+        '''
+        returns a queryset of current category and it's descendants.
+        '''
+        category = self.get_category()
+        return category.get_descendants(include_self=True)
 
     def get(self, request, *args, **kwargs):
         if request.GET.get('ajax', None):
             qs = self.get_dated_items()[1]
-            category_id = kwargs.get("pk")
-            category = self.get_category(category_id)
+            category = self.get_category()
             data = get_subcategory_stats(qs, category)
             return JsonResponse(data)
         return super().get(request, *args, **kwargs)
