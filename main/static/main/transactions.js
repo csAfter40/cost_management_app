caea0560f4eb44d7af4cb7c7af02a2e62fc9dd73
csAfter40 <csafter40@gmail.com>
2022-10-26 14:15:04 +0300

    chore: add transactions js file

---
+++ main/static/main/transactions.js
@@ -0,0 +1,25 @@
+import {setupDeleteButtons} from "./delete_button_handler.js";
+import { setupTimeButtons } from "./time_button_handler.js";
+import {setupPgButtons} from "./paginator_buttons_handler.js";
+
+const tablePaginatorGroup = document.querySelector('#table-paginator-group');
+const timeButtonsDiv = document.querySelector('#time-buttons-div')
+
+setupTimeButtons(getData);
+setupPgButtons(getData);
+
+function getData(page = 1) {
+    const path = timeButtonsDiv.dataset.path;
+    const url = window.location.pathname + path + `?page=${page}`
+
+    fetch(url, {
+        method: "GET",
+        headers: {}
+    }).then(response => {
+        return response.text();
+    }).then(data => {
+        tablePaginatorGroup.innerHTML = data;
+        setupPgButtons(getData);
+        setupDeleteButtons();
+    });
+};
