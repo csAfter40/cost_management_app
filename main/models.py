f5abeeb5951e053666d5ca94a6deebe31e70f6d8
csAfter40 <csafter40@gmail.com>
2022-10-27 22:41:55 +0300

    chore: add related name for transactions field in category model

--- main/models.py
+++ main/models.py
@@ -85,7 +85,7 @@ class Transaction(models.Model):
     name = models.CharField(max_length=128)
     amount = models.DecimalField(max_digits=14, decimal_places=2)
     date = models.DateField(blank=True, default=date.today)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='transactions')
     type = models.CharField(max_length=2, choices=TRANSACTION_TYPES)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
