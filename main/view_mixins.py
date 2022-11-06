294c52802e511223540792af548f70610e6e5305
csAfter40 <csafter40@gmail.com>
2022-11-06 09:07:02 +0300

    chore: edit category date archive mixin for performance improvement

--- main/view_mixins.py
+++ main/view_mixins.py
@@ -6,6 +6,7 @@ from .utils import (
     get_ins_outs_report, 
     get_report_total,
     get_subcategory_stats,
+    get_multi_currency_category_stats,
     get_multi_currency_main_category_stats,
     get_multi_currency_category_detail_stats,
 )
@@ -47,18 +48,16 @@ class InsOutsDateArchiveMixin():
 
 class CategoryDateArchiveMixin():
     def get_queryset(self):
-        user_accounts_list = Account.objects.filter(user=self.request.user).values_list('pk', flat=True)
-        user_transfer_categories = Category.objects.filter(user=self.request.user, is_transfer=True)
         descendant_categories = self.get_category_descendants()
         return super().get_queryset().filter(
-            content_type__model='account', 
-            object_id__in=user_accounts_list,
-            category__in=descendant_categories
-            ).exclude(category__in=user_transfer_categories)
+            account__user=self.request.user, category__in=descendant_categories
+            ).exclude(category__is_transfer=True
+            ).select_related('category').prefetch_related('content_object__currency')
 
     def get_category(self):
         category_id = self.kwargs.get("pk")
-        return get_object_or_404(Category, pk=category_id)
+        self.category = get_object_or_404(Category, pk=category_id)
+        return self.category
 
     def get_category_descendants(self):
         '''
@@ -68,13 +67,12 @@ class CategoryDateArchiveMixin():
         return category.get_descendants(include_self=True)
 
     def get_context_data(self, **kwargs):
-        category = self.get_category()
         transactions = self.get_dated_items()[1]
-        category_stats = get_multi_currency_category_detail_stats(
-            transactions, category, self.request.user.primary_currency
+        category_stats = get_multi_currency_category_stats(
+            transactions, self.category, self.request.user
         )
         kwargs.update({
-            'category': self.get_category(),
+            'category': self.category,
             'category_stats': category_stats,
             'date': datetime.date.today(),
             'table_template': 'main/table_transactions.html',
