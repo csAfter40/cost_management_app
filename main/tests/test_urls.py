6fb0814edcb4fd3c53e29ba43b9694a199dd220d
csAfter40 <csafter40@gmail.com>
2022-11-07 09:24:12 +0300

    test: add test for subcatecory stats all archive url

--- main/tests/test_urls.py
+++ main/tests/test_urls.py
@@ -140,6 +140,11 @@ class TestUrls(SimpleTestCase):
             "/categories/1/date/2001/1/1", views.CategoryDayArchiveView, "category_day_archive", pk=1, year=2001, month=1, day=1
         )
 
+    def test_subcategory_stats_all_archive_url(self):
+        self.assert_path_resolves_to_CBV(
+            "/subcategories/1/date", views.SubcategoryStatsAllArchiveView, "subcategory_all_archive", pk=1
+        )
+
     def test_expense_category_create_url(self):
         self.assert_path_resolves_to_CBV(
             "/categories/expense/create",
