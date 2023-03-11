afb578b1a3ec656d62414732f2c8987c4e33465f
csAfter40 <csafter40@gmail.com>
2023-03-11 20:39:50 +0300

    chore: edit initial categories. used ai generated category names.

--- main/categories.py
+++ main/categories.py
@@ -1,56 +1,98 @@
-expense_categories = {
-    "Automobile": {
-        "slug": "automobile",
-        "children": {
-            "Insurance": {"slug": "insurance", "children": None},
-            "Maintenance": {"slug": "maintenance", "children": None},
-            "Gas": {"slug": "gas", "children": None},
-        },
-    },
-    "Food": {
-        "slug": "food",
-        "children": {
-            "Groceries": {"slug": "groceries", "children": None},
-            "Restaurant": {"slug": "restaurant", "children": None},
-        },
-    },
-    "Uncategorized": {"slug": "uncategorized-expense", "children": None},
-}
-
-income_categories = {
-    "Salary": {"slug": "salary", "children": None},
-    "Rents": {"slug": "rents", "children": None},
-    "Bank": {"slug": "bank", "children": None},
-    "Uncategorized": {"slug": "uncategorized-income", "children": None},
-}
-
 categories = {
-    # Expense Categories
-    "Automobile": {
+    # Expense categories
+    "Housing": {
         "type": "E",
         "children": {
+            "Mortgage/Rent": {"type": "E", "children": None},
+            "Taxes": {"type": "E", "children": None},
             "Insurance": {"type": "E", "children": None},
-            "Maintenance": {"type": "E", "children": None},
-            "Gas": {"type": "E", "children": None},
-        },
+            "Repairs": {"type": "E", "children": None},
+            "Utilities": {"type": "E", "children": None}
+        }
+    },
+    "Transportation": {
+        "type": "E",
+        "children": {
+            "Car Payments": {"type": "E", "children": None},
+            "Gas/Fuel": {"type": "E", "children": None},
+            "Insurance": {"type": "E", "children": None},
+            "Repairs": {"type": "E", "children": None},
+            "Public Transit": {"type": "E", "children": None}
+        }
     },
     "Food": {
         "type": "E",
         "children": {
             "Groceries": {"type": "E", "children": None},
-            "Restaurant": {"type": "E", "children": None},
-        },
+            "Dining Out": {"type": "E", "children": None},
+            "Alcohol": {"type": "E", "children": None},
+            "Snacks": {"type": "E", "children": None}
+        }
     },
-    "Uncategorized": {"type": "E", "children": None},
+    "Personal Care": {
+        "type": "E",
+        "children": {
+            "Haircuts": {"type": "E", "children": None},
+            "Toiletries": {"type": "E", "children": None},
+            "Clothing": {"type": "E", "children": None},
+            "Gym Memberships": {"type": "E", "children": None}
+        }
+    },
+    "Health": {
+        "type": "E",
+        "children": {
+            "Insurance": {"type": "E", "children": None},
+            "Medicine": {"type": "E", "children": None},
+            "Medical Supplies": {"type": "E", "children": None}
+        }
+    },
+    "Education": {
+        "type": "E",
+        "children": {
+            "Tuition": {"type": "E", "children": None},
+            "Books": {"type": "E", "children": None},
+            "Activities": {"type": "E", "children": None},
+        }
+    },
+    "Entertainment": {
+        "type": "E",
+        "children": {
+            "Movies": {"type": "E", "children": None},
+            "TV": {"type": "E", "children": None},
+            "Streaming Services": {"type": "E", "children": None},
+            "Hobbies": {"type": "E", "children": None}
+        }
+    },
+    "Gifts/Donations": {
+        "type": "E",
+        "children": {
+            "Gifts": {"type": "E", "children": None},
+            "Donations": {"type": "E", "children": None},
+            "Celebrations": {"type": "E", "children": None}
+        }
+    },
+    "Miscellaneous": {
+        "type": "E",
+        "children": {
+            "Bank Fees": {"type": "E", "children": None},
+            "Taxes": {"type": "E", "children": None},
+            "Legal Fees": {"type": "E", "children": None}
+        }
+    },
+    # Income categories
+    "Salary": {"type": "I", "children": None},
+    "Investments": {"type": "I", "children": None},
+    "Rental Income": {"type": "I", "children": None},
+    "Child Support": {"type": "I", "children": None},
+    "Other Income": {"type": "I", "children": None},
+    "Bank Interests": {"type": "I", "children": None},
+    "Scholarship": {"type": "I", "children": None},
+    "Gifts": {"type": "I", "children": None},
+    # Protected categories
     "Transfer Out": {"type": "E", "children": None, "is_transfer": True, 'is_protected': True},
     "Pay Loan": {"type": "E", 'children': None, 'is_protected': True},
     "Pay Card": {"type": "E", 'children': None, 'is_protected': True},
     "Asset Delete": {"type": "E", "children":None, "is_protected": True},
-    # Income Categories
-    "Salary": {"type": "I", "children": None},
-    "Rents": {"type": "I", "children": None},
-    "Bank": {"type": "I", "children": None},
-    "Uncategorized": {"type": "I", "children": None},
     "Transfer In": {"type": "I", "children": None, "is_transfer": True, 'is_protected': True},
     "Loan In": {"type": 'I', 'children': None, 'is_protected': True},
     "Pay Loan": {"type": "I", 'children': None, 'is_protected': True},
