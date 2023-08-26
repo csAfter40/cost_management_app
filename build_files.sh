6512b7c7a9af85577d02b68d02fa79891860e205
csAfter40 <csafter40@gmail.com>
2023-08-26 23:32:10 +0300

    chore: add collectstatic command

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,5 @@ pip install wheel
 pip install -r requirements.txt
 
 # python manage.py migrate
-# python3.9 manage.py collectstatic --no-input
+python3.9 manage.py collectstatic --no-input
 # python manage.py loaddata currencies.json
